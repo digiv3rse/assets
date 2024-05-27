5559ac39eb24a6f513a3d1162f0ded51639041e3
DiGiV3rse <kimhabork@icloud.com>
2024-05-27 19:48:07 +0700

    Update urls.ts

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,8 +1,8 @@
-export const APP_URL = "https://app.digiv3rse.xyz";
+export const APP_URL = "https://app.digiv3rse.social";
 export const TWITTER_URL = "https://twitter.com/digiv3rse";
-export const DOCUMENTATION_URL = "https://docs.digiv3rse.xyz";
+export const DOCUMENTATION_URL = "https://docs.digiv3rse.social";
 export const CONTRACTS_BOOK_URL =
-  "https://docs.digiv3rse.xyz/contracts";
+  "https://docs.digiv3rse.social/contracts";
 export const SDK_DOCUMENTATION_URL =
-  "https://docs.digiv3rse.xyz/sdk/overview";
+  "https://docs.digiv3rse.social/sdk/overview";
 export const GITHUB_URL = "https://github.com/digiv3rse";
